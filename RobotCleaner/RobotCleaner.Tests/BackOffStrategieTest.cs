830dbeca62210c73b73b67953c533c6690506da2
Roman Zolotarev <mirom90@gmail.com>
2018-05-23 11:41:34 +0300

    minor refactoring

--- RobotCleaner/RobotCleaner.Tests/BackOffStrategieTest.cs
+++ RobotCleaner/RobotCleaner.Tests/BackOffStrategieTest.cs
@@ -51,8 +51,6 @@ namespace RobotCleaner.Tests
                 currentCommand = currentCommand.Next;
             }
 
-            var visited = container.Resolve<Tracker>().Visited;
-            var cleaned = container.Resolve<Tracker>().Cleaned;
             Assert.AreEqual(54, positionState.LeftBattery);
             Assert.AreEqual(2, positionState.X);
             Assert.AreEqual(0, positionState.Y);
@@ -60,7 +58,7 @@ namespace RobotCleaner.Tests
         }
 
         [Test]
-        public void Test2BackOffStrategy()
+        public void Test2BackOffStrategy_Position()
         {
             var container = Container.BuildContainer();
 
@@ -95,6 +93,48 @@ namespace RobotCleaner.Tests
                 currentCommand = currentCommand.Next;
             }
 
+            Assert.AreEqual(1040, positionState.LeftBattery);
+            Assert.AreEqual(3, positionState.X);
+            Assert.AreEqual(2, positionState.Y);
+            Assert.AreEqual(Facing.E, positionState.Facing);
+        }
+
+        [Test]
+        public void Test2BackOffStrategy_VisitedPoints()
+        {
+            var container = Container.BuildContainer();
+
+            var map = new string[4, 4] { { "S", "S", "S", "S" },{ "S", "S", "C", "S" },
+                                         { "S", "S", "S", "S" }, {"S", "null", "S", "S"} };
+            var newMap = map.ToPlaceStatuses().Matrix;
+            var positionState = container.Resolve<PositionState>();
+            positionState.Facing = Facing.S;
+            positionState.X = 3;
+            positionState.Y = 1;
+            positionState.LeftBattery = 1094;
+            positionState.SetMap(newMap);
+
+            var commands = new LinkedList<Contracts.Commands.Instructions>();
+            commands.AddLast(Contracts.Commands.Instructions.TR);
+            commands.AddLast(Contracts.Commands.Instructions.A);
+            commands.AddLast(Contracts.Commands.Instructions.C);
+            commands.AddLast(Contracts.Commands.Instructions.A);
+            commands.AddLast(Contracts.Commands.Instructions.C);
+            commands.AddLast(Contracts.Commands.Instructions.TR);
+            commands.AddLast(Contracts.Commands.Instructions.A);
+            commands.AddLast(Contracts.Commands.Instructions.C);
+
+            var instructionExecutor = container.Resolve<IInstructionExecutor>();
+            var currentCommand = commands.First;
+            while (currentCommand != null)
+            {
+                var isSuccessful = instructionExecutor.TryToExecuteInstruction(currentCommand.Value);
+                if (!isSuccessful)
+                    break;
+
+                currentCommand = currentCommand.Next;
+            }
+
             var actulaVisited = container.Resolve<Tracker>().Visited;
             var expectedVisited = new List<Coordinate>()
             {
@@ -105,21 +145,53 @@ namespace RobotCleaner.Tests
             };
             Assert.AreEqual(expectedVisited.Count, actulaVisited.Count);
             actulaVisited.ForEach(a => Assert.That(expectedVisited.Contains(a)));
+        }
+
+        [Test]
+        public void Test2BackOffStrategy_CleanedPoints()
+        {
+            var container = Container.BuildContainer();
+
+            var map = new string[4, 4] { { "S", "S", "S", "S" },{ "S", "S", "C", "S" },
+                                         { "S", "S", "S", "S" }, {"S", "null", "S", "S"} };
+            var newMap = map.ToPlaceStatuses().Matrix;
+            var positionState = container.Resolve<PositionState>();
+            positionState.Facing = Facing.S;
+            positionState.X = 3;
+            positionState.Y = 1;
+            positionState.LeftBattery = 1094;
+            positionState.SetMap(newMap);
+
+            var commands = new LinkedList<Contracts.Commands.Instructions>();
+            commands.AddLast(Contracts.Commands.Instructions.TR);
+            commands.AddLast(Contracts.Commands.Instructions.A);
+            commands.AddLast(Contracts.Commands.Instructions.C);
+            commands.AddLast(Contracts.Commands.Instructions.A);
+            commands.AddLast(Contracts.Commands.Instructions.C);
+            commands.AddLast(Contracts.Commands.Instructions.TR);
+            commands.AddLast(Contracts.Commands.Instructions.A);
+            commands.AddLast(Contracts.Commands.Instructions.C);
+
+            var instructionExecutor = container.Resolve<IInstructionExecutor>();
+            var currentCommand = commands.First;
+            while (currentCommand != null)
+            {
+                var isSuccessful = instructionExecutor.TryToExecuteInstruction(currentCommand.Value);
+                if (!isSuccessful)
+                    break;
+
+                currentCommand = currentCommand.Next;
+            }
 
             var expectedCleaned = new List<Coordinate>()
             {
                 new Coordinate(2,2),
-                new Coordinate(3,0),                
+                new Coordinate(3,0),
                 new Coordinate(3,2)
             };
             var actualCleaned = container.Resolve<Tracker>().Cleaned;
             Assert.AreEqual(expectedCleaned.Count, actualCleaned.Count);
             actualCleaned.ForEach(a => Assert.That(expectedCleaned.Contains(a)));
-
-            Assert.AreEqual(1040, positionState.LeftBattery);
-            Assert.AreEqual(3, positionState.X);
-            Assert.AreEqual(2, positionState.Y);
-            Assert.AreEqual(Facing.E, positionState.Facing);
-        }  
+        }
     }
 }
